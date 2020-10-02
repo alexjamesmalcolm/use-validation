bf65e2542e45b2f5da489aaeacf1f090150f18ad
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-02 17:49:12 -0400

    Added features for working with unique inputs while preventing controlled input bug from reappearing

--- src/lib/useValidation/index.tsx
+++ src/lib/useValidation/index.tsx
@@ -16,48 +16,47 @@ interface Options {
 
 const useValidation = (validator: Validator, options: Options = {}) => {
   const parent = useRef<HTMLDivElement>(null);
-  const {
-    debounceWait,
-    getInputFromWrapper = (wrapper: HTMLDivElement) =>
-      wrapper.children[0] as HTMLInputElement,
-    getValueFromInput,
-  } = options;
+  const debounceWait = useMemo(() => options.debounceWait || 0, [
+    options.debounceWait,
+  ]);
+  const getValueFromInput = useMemo(
+    () =>
+      options.getValueFromInput || ((input: HTMLInputElement) => input.value),
+    [options.getValueFromInput]
+  );
+  const getInputFromWrapper = useMemo(
+    () =>
+      options.getInputFromWrapper ||
+      ((wrapper: HTMLDivElement) => wrapper.children[0] as HTMLInputElement),
+    [options.getInputFromWrapper]
+  );
   const actOnInput = useCallback(
     (callback: (input: HTMLInputElement) => void) => {
       parent.current && callback(getInputFromWrapper(parent.current));
     },
     [getInputFromWrapper]
   );
-  const setMessage = useCallback<(message: string) => void>(
+  const setMessage = useCallback(
     (message: string) => {
       actOnInput((input) => input.setCustomValidity(message));
     },
     [actOnInput]
   );
-  const debounced = useMemo(
-    () =>
-      debounce((callback: Function) => {
-        callback();
-      }, debounceWait || 0),
-    [debounceWait]
+  const debouncedValidation = useCallback(
+    debounce((resolve: (value?: unknown) => void) => {
+      actOnInput((input) => resolve(validator(getValueFromInput(input))));
+    }, debounceWait),
+    []
   );
-  const checkValidity = useCallback<() => Promise<unknown>>(
+  const checkValidity = useCallback(
     () =>
       new Promise((resolve) => {
         setMessage("Validating...");
-        debounced(() => {
-          actOnInput((input) => {
-            resolve(
-              validator(
-                getValueFromInput ? getValueFromInput(input) : input.value
-              )
-            );
-          });
-        });
+        debouncedValidation(resolve);
       }),
-    [actOnInput, debounced, getValueFromInput, setMessage, validator]
+    [debouncedValidation, setMessage]
   );
-  const reportValidity = useCallback<() => void>(() => {
+  const reportValidity = useCallback(() => {
     checkValidity().then((validityCheckResult) => {
       setMessage(
         typeof validityCheckResult === "string"
@@ -70,7 +69,7 @@ const useValidation = (validator: Validator, options: Options = {}) => {
   }, [checkValidity, setMessage]);
   useEffect(() => {
     reportValidity();
-  }, [reportValidity]);
+  }, [reportValidity, validator]);
   return useCallback(
     ({ children }) => (
       <div onChangeCapture={reportValidity} ref={parent} style={style}>
