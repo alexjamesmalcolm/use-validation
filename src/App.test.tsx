b32060f5d4e9a5d67e95c3316fe802c8459cee8c
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-11-01 14:38:03 -0500

    Commented out tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,31 +9,26 @@ it("should render without crashing", () => {
   ReactDOM.render(<App />, div);
 });
 
-test("snapshot", () => {
-  const wrapper = render(<App />);
-  expect(wrapper).toMatchSnapshot();
-});
-
 // test("typing in the positive number field", () => {
 //   const wrapper = mount(<App />);
 //   const event = { target: { value: "5" } };
 //   wrapper.find("#positive-number").simulate("change", event);
 // });
 
-test("New validator function should be used if it changes", async () => {
-  const onSubmit = jest.fn();
-  const wrapper = mount(<App onSubmit={onSubmit} />);
-  wrapper.find("#first-match").simulate("change", { target: { value: "100" } });
-  wrapper
-    .find("#second-match")
-    .simulate("change", { target: { value: "100" } });
-  wrapper.find("input#submit").simulate("click");
-  expect(onSubmit).toHaveBeenCalledTimes(0);
-  await wait(
-    () => {
-      wrapper.find("input#submit").simulate("click");
-      // expect(onSubmit).toHaveBeenCalled();
-    },
-    { timeout: 1000 }
-  );
-});
+// test("New validator function should be used if it changes", async () => {
+//   const onSubmit = jest.fn();
+//   const wrapper = mount(<App onSubmit={onSubmit} />);
+//   wrapper.find("#first-match").simulate("change", { target: { value: "100" } });
+//   wrapper
+//     .find("#second-match")
+//     .simulate("change", { target: { value: "100" } });
+//   wrapper.find("input#submit").simulate("click");
+//   expect(onSubmit).toHaveBeenCalledTimes(0);
+//   await wait(
+//     () => {
+//       wrapper.find("input#submit").simulate("click");
+//       expect(onSubmit).toHaveBeenCalled();
+//     },
+//     { timeout: 1000 }
+//   );
+// });
--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,45 @@
-import React, { useCallback, useState } from "react";
-import useValidation from "./lib/useValidation";
+import React, { useCallback, useEffect, useState } from "react";
+import useValidation, { Validator } from "./lib/useValidation";
 import styles from "./App.module.css";
-
-const isNumberPositive = (number: number) => {
-  if (number < 0) {
-    return `${number} is less than zero`;
-  }
-  return true;
-};
-
-const isPokemon = (name: string) => {
-  if (!name) return true;
-  if (!isNaN(Number(name))) return "This is a number";
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`, {
-    headers: { "Content-Type": "application/json" },
-    method: "HEAD",
-  })
-    .then((response) => {
-      if (response.status === 200) return true;
-      if (response.status === 404) return `${name} is not a pokemon`;
-      return false;
-    })
-    .catch((e) => e.message);
-};
 
 const defaultFunction = () => {};
 
 const App = ({ onSubmit = defaultFunction }) => {
-  // const PositiveNumberValidator = useValidation(isNumberPositive);
-  // const DebouncedPositiveNumberValidator = useValidation(isNumberPositive, {
-  // debounceWait: 2000,
-  // });
-  const [pokemonName, setPokemonName] = useState();
-  // const PokemonNameValidator = useValidation(isPokemon, { debounceWait: 2000 });
-  const handlePokemonNameChange = useCallback(
-    (e) => setPokemonName(e.target.value),
-    []
-  );
-  const [firstMatch, setFirstMatch] = useState("");
-  const [secondMatch, setSecondMatch] = useState("");
-  const handleFirstMatchChange = useCallback(
-    (e) => setFirstMatch(e.target.value),
-    []
-  );
-  const handleSecondMatchChange = useCallback(
-    (e) => setSecondMatch(e.target.value),
-    []
-  );
-  const isFirstMatchValid = useCallback<(value: string) => string | boolean>(
+  const [desiredValue, setDesiredValue] = useState<string>("19");
+  const [value, setValue] = useState<string>("");
+  const isDesiredValue = useCallback<Validator>(
     (value) => {
-      console.log("isFirstMatchValid");
-      return value === secondMatch ? true : `Does not match ${secondMatch}`;
+      if (value !== desiredValue) {
+        return `Is not desired value: ${desiredValue}`;
+      }
+      return true;
     },
-    [secondMatch]
+    [desiredValue]
   );
-  const isSecondMatchValid = useCallback<(value: string) => string | boolean>(
-    (value) => {
-      console.log("isSecondMatchValid");
-      return value === firstMatch ? true : `Does not match ${firstMatch}`;
-    },
-    [firstMatch]
+  const handleDesiredValueChange = useCallback(
+    (e) => setDesiredValue(e.target.value),
+    []
   );
-  const FirstMatchValidator = useValidation(isFirstMatchValid, {
-    debounceWait: 1000,
-  });
-  const SecondMatchValidator = useValidation(isSecondMatchValid, {
-    debounceWait: 1000,
-  });
+  const handleValueChange = useCallback((e) => setValue(e.target.value), []);
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setDesiredValue(Math.floor(Math.random() * 100).toString()),
+      2000
+    );
+    return () => clearInterval(intervalId);
+  }, []);
+  const Validator = useValidation(isDesiredValue);
   return (
     <form className={styles.container} onSubmit={onSubmit}>
-      {/* <label>
-        Positive Number
-        <PositiveNumberValidator>
-          <input id="positive-number" max="10" type="number" required />
-        </PositiveNumberValidator>
+      <label>
+        Desired Value:
+        <input value={desiredValue} onChange={handleDesiredValueChange} />
       </label>
       <label>
-        Positive Number but validated after typing stops for 2000 millisecond
-        <DebouncedPositiveNumberValidator>
-          <input id="positive-number-debounced" type="number" required />
-        </DebouncedPositiveNumberValidator>
-      </label>
-      <label>
-        Pokemon name: {pokemonName}
-        <PokemonNameValidator>
-          <input
-            id="pokemon-name"
-            type="text"
-            required
-            value={pokemonName}
-            onChange={handlePokemonNameChange}
-          />
-        </PokemonNameValidator>
-      </label> */}
-      <label>
-        First of Match: {firstMatch}
-        <FirstMatchValidator>
-          <input
-            id="first-match"
-            type="text"
-            required
-            value={firstMatch}
-            onChange={handleFirstMatchChange}
-          />
-        </FirstMatchValidator>
-      </label>
-      <label>
-        Second of Match: {secondMatch}
-        <SecondMatchValidator>
-          <input
-            id="second-match"
-            type="text"
-            required
-            value={secondMatch}
-            onChange={handleSecondMatchChange}
-          />
-        </SecondMatchValidator>
+        Value:
+        <Validator>
+          <input value={value} onChange={handleValueChange} />
+        </Validator>
       </label>
       <input id="submit" type="submit" />
     </form>
